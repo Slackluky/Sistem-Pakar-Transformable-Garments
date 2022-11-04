164c0c10d8b4b20f9a5bc06e6171c2d2899eee31
Slackluky <unluckly17@gmail.com>
2022-11-05 04:14:59 +0700

    zig

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,9 +9,9 @@
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
         @routes
-        <script defer>
+        {{-- <script defer>
             Ziggy.url = '{{ env('APP_URL') }}'
-        </script>
+        </script> --}}
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
